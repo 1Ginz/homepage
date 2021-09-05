a5f628fb9499d235bf1ba9298d50f76cb6836771
1Ginz <hunglq2908@gmail.com>
2021-09-05 12:21:18 +0700

    add header-footer

---
+++ source/product-script.js
@@ -0,0 +1,6 @@
+let info = document.getElementsByClassName('choice-info');
+let comment = document.getElementsByClassName('choice-comment');
+let infoContent = document.getElementsByClassName('more-info');
+let commentContent = document.getElementsByClassName('comment')
+
+
