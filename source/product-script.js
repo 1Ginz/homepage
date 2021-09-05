1d3fddfb1bb5bd0b7bd7686a43cb25fd32c9c912
1Ginz <hunglq2908@gmail.com>
2021-09-05 12:39:35 +0700

    ko add event dc

--- source/product-script.js
+++ source/product-script.js
@@ -1,6 +1,16 @@
-let info = document.getElementsByClassName('choice-info');
-let comment = document.getElementsByClassName('choice-comment');
-let infoContent = document.getElementsByClassName('more-info');
-let commentContent = document.getElementsByClassName('comment')
+let info = document.getElementsById('1');
+let comment = document.getElementsById('2');
+let infoContent = document.getElementsById('3');
+let commentContent = document.getElementsById('4');
 
+function showInfo(){
+    infoContent.style.display = 'block'; 
+    commentContent.styte.display =  'none';
+  }
+function showComment(){
+    infoContent.styte.display =  "none";
+    commentContent.styte.display = "block";
+}
 
+info.addEventListener('click',showInfo);
+comment.addEventListener('click',showComment);
